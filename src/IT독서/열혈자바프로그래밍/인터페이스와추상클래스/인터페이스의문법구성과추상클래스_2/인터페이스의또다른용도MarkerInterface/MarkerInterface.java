f5c65bdaa31de24e1a59486b2a3e5e8a7c1de5ac
김충섭 <kchs94@gmail.com>
2021-07-19 16:49:41 +0900

    Add: MarkerInterface.java

---
+++ src/IT독서/열혈자바프로그래밍/인터페이스와추상클래스/인터페이스의문법구성과추상클래스_2/인터페이스의또다른용도MarkerInterface/MarkerInterface.java
@@ -0,0 +1,12 @@
+package IT독서.열혈자바프로그래밍.인터페이스와추상클래스.인터페이스의문법구성과추상클래스_2.인터페이스의또다른용도MarkerInterface;
+/*
+* 마커인터페이스(Marker Interface)
+* 특징 1.아무런 메소드도 없는 경우가 많음.
+* 특징 2.클래스에 특별한 표식을 다는 목적으로 사용되는 인터페이스
+*
+* */
+public class MarkerInterface {
+    public static void main(String[] args) {
+        
+    }
+}
