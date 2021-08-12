128a347ae86d46c1c5d35a88a8ead165fdcee2be
김충섭 <kchs94@gmail.com>
2021-08-12 15:27:33 +0900

    Add: 챕26_네스티드와람다

---
+++ src/IT독서/열혈자바프로그래밍/챕26_네스티도와람다/src/Lambda1/Lambda1.java
@@ -0,0 +1,18 @@
+package IT독서.열혈자바프로그래밍.챕26_네스티도와람다.src.Lambda1;
+
+interface Printable{
+    void print(String s);
+}
+
+class Printer implements Printable {
+    public void print(String s){
+        System.out.println(s);
+    }
+}
+
+public class Lambda1 {
+    public static void main(String[] args) {
+        Printable prn = new Printer();
+        prn.print("람다가 뭐에요?");
+    }
+}
---
+++ src/IT독서/열혈자바프로그래밍/챕26_네스티도와람다/src/Lambda4/Lambda4.java
@@ -0,0 +1,15 @@
+package IT독서.열혈자바프로그래밍.챕26_네스티도와람다.src.Lambda4;
+
+interface Printable{
+    void print(String s);
+}
+
+public class Lambda4 {
+    public static void ShowString(Printable p, String s){
+        p.print(s);
+    }
+
+    public static void main(String[] args) {
+        ShowString((s) -> {System.out.println(s);}, "람다가뭐에요?");
+    }
+}
