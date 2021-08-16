25dae182245c0627830d074e649d229af7977ee6
김충섭 <kchs94@gmail.com>
2021-08-16 17:07:12 +0900

    Add 열혈_챕27_람다표현식

---
+++ src/IT독서/열혈자바프로그래밍/챕27_람다표현식/src/TwoParamAndReturn/TwoParamAndReturn.java
@@ -0,0 +1,16 @@
+package IT독서.열혈자바프로그래밍.챕27_람다표현식.src.TwoParamAndReturn;
+
+interface Calculate{
+    int cal(int a, int b);  // 값을 반환하는 추상 메소드
+}
+
+public class TwoParamAndReturn {
+    public static void main(String[] args) {
+        Calculate c;
+        c = (a, b) -> { return a + b;};
+        System.out.println(c.cal(4, 3));
+
+        c = (a, b) -> a + b;
+        System.out.println(c.cal(4, 3));    
+    }
+}
