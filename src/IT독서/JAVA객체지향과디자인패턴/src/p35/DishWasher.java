2c93ae993107e32dac4f1dc5ba5ee7a23375b2d0
김충섭 <kchs94@gmail.com>
2021-09-09 13:03:40 +0900

    Add 디자인패턴 p35

---
+++ src/IT독서/JAVA객체지향과디자인패턴/src/p35/DishWasher.java
@@ -0,0 +1,13 @@
+package IT독서.JAVA객체지향과디자인패턴.src.p35;
+
+public class DishWasher extends HomeAppliancies {
+    @Override
+    public void turnOn(){
+
+    }
+
+    @Override
+    public void turnOff(){
+
+    }
+}
---
+++ src/IT독서/JAVA객체지향과디자인패턴/src/p35/HomeAppliancies.java
@@ -0,0 +1,10 @@
+package IT독서.JAVA객체지향과디자인패턴.src.p35;
+
+public abstract class HomeAppliancies {
+    private int serialNo;
+    private String manufacturer;
+    private int year;
+
+    public abstract void turnOn();
+    public abstract void turnOff();
+}
