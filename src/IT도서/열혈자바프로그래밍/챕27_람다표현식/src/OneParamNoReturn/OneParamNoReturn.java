ff51b72a1fb67fb404e3cd5cbc4a654f7b310a72
김충섭 <kchs94@gmail.com>
2021-10-07 16:14:22 +0900

    Update 매개변수가 1개이고 반환이 없는 람다식의 줄임방법

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/OneParamNoReturn/OneParamNoReturn.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/OneParamNoReturn/OneParamNoReturn.java
@@ -1,22 +1,23 @@
 package IT도서.열혈자바프로그래밍.챕27_람다표현식.src.OneParamNoReturn;
 
-interface Printable{
-    void print(String s);   // 매개변수 하나, 반환형 void
+interface Printable {
+    void print(String message); // 인터페이스 안에 추상메소드 1개만 존재.
 }
 
 public class OneParamNoReturn {
     public static void main(String[] args) {
-        Printable p;
-        p = (String s) -> {System.out.println(s);};  // 줄임표현 없는 람다식 표현
-        p.print("람다 예시 1");
+        Printable printable;
 
-        p = (String s) -> System.out.println(s);    // 메소드 몸체에 한 문장이니 중괄호 생략 가능.
-        p.print("람다 예시 2");
+        printable = (String message) -> { System.out.println(message);};    // 줄임표현이 없는 람다식
+        printable.print("줄임 표현이 없는 람다식");
 
-        p = (s) -> System.out.println(s);           // 매개변수 자료형은 인터페이스에 있으므로 삭제 가능.
-        p.print("람다 예시 3");
+        printable = (String message) -> System.out.println(message);        // 한 문장이라서 중괄호를 생략할 수 있는 람다식
+        printable.print("중괄호를 생략한 람다식");
 
-        p = s -> System.out.print(s);               // 매개변수 갯수가 1개 이므로 소괄호 생략 가능.
+        printable = (message) -> System.out.println(message);       // 매개변수 형을 생략한 람다식
+        printable.print("매개변수 형을 생략한 람다식");
 
+        printable = message -> System.out.println(message);     // 매개변수가 1개라서 소괄호를 생략한 람다식.
+        printable.print("매개변수 소괄호를 생략한 람다식");
     }
 }
