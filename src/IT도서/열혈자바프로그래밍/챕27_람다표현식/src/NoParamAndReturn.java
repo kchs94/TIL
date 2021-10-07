f31bfa42a56f63ba9d594fa25688f85e06c20ec0
김충섭 <kchs94@gmail.com>
2021-10-07 16:46:27 +0900

    Update 매개변수가 있고 반환하는 람다식의 줄임

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/NoParamAndReturn.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/NoParamAndReturn.java
@@ -3,16 +3,16 @@ package IT도서.열혈자바프로그래밍.챕27_람다표현식.src;
 import java.util.Random;
 
 interface Generator {
-    int rand(); // 매개변수 없는 메소드
+    int getRandomNum(); // 매개변수 없는 메소드
 }
 
 public class NoParamAndReturn {
     public static void main(String[] args) {
-        Generator gen = () -> { // 람다식
-            Random rand = new Random();
-            return rand.nextInt(50);
+        Generator generator = () -> {
+            Random random = new Random();
+            return random.nextInt(50);
         };
 
-        System.out.println(gen.rand());
+        System.out.println(generator.getRandomNum());
     }
 }
