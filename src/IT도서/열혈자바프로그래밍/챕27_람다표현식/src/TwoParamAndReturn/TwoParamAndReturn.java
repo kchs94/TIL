511fdf036702b901fbd7cd2026a9676df662004e
김충섭 <kchs94@gmail.com>
2021-10-07 16:30:08 +0900

    Update 매개변수가 있고 반환하는 람다식의 줄임

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/TwoParamAndReturn/TwoParamAndReturn.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/TwoParamAndReturn/TwoParamAndReturn.java
@@ -1,16 +1,17 @@
 package IT도서.열혈자바프로그래밍.챕27_람다표현식.src.TwoParamAndReturn;
 
 interface Calculate{
-    int cal(int a, int b);  // 값을 반환하는 추상 메소드
+    int calculate(int a, int b);  // 값을 반환하는 추상 메소드
 }
 
 public class TwoParamAndReturn {
     public static void main(String[] args) {
-        Calculate c;
-        c = (a, b) -> { return a + b;};
-        System.out.println(c.cal(4, 3));
+        Calculate calculator;
 
-        c = (a, b) -> a + b;
-        System.out.println(c.cal(4, 3));    
+        calculator = (a, b) -> { return a+b ;};
+        System.out.println( calculator.calculate(3,4));
+
+        calculator = (a, b) -> a*b;
+        System.out.println(calculator.calculate(2,5));
     }
 }
