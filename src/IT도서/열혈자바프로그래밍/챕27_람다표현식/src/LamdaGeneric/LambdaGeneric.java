15258c9ae83d518550b8c2daee5f47fe8b35cca2
김충섭 <kchs94@gmail.com>
2021-10-10 16:15:04 +0900

    Update LambdaGeneric.java

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/LamdaGeneric/LambdaGeneric.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/LamdaGeneric/LambdaGeneric.java
@@ -2,16 +2,16 @@ package IT도서.열혈자바프로그래밍.챕27_람다표현식.src.LamdaGene
 // 함수형 인터페이스: 추상 메소드를 1개 가진 인터페이스. 메소드가 여러 개 있어도 괜찮음.
 
 @FunctionalInterface
-interface Calculate <T>{
+interface Calculable<T>{    // 제네릭 인터페이스
     T calculate(T a, T b);
 }
 
 public class LambdaGeneric {
     public static void main(String[] args) {
-        Calculate<Integer> calculator = (a, b) -> a+b;
-        System.out.println(calculator.calculate(3,5));
+        Calculable<Integer> calculator = (a, b) -> a+b;
+        System.out.println(calculator.calculate(10, 5));
 
-        Calculate<Double> doubleCalculate = (a, b) -> a+b;
-        System.out.println(doubleCalculate.calculate(3.42, 4.123));
+        Calculable<Double> calculatorDouble = (a, b) -> a+b;
+        System.out.println(calculatorDouble.calculate(3.14, 4.12));
     }
 }
