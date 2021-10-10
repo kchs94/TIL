1eda376f93c046eaad808a8ffab9d421b179f735
김충섭 <kchs94@gmail.com>
2021-10-10 15:45:12 +0900

    Update TwoParamNoReturn.java

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/TwoParamAndReturn/TwoParamAndReturn.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/TwoParamAndReturn/TwoParamAndReturn.java
@@ -1,17 +1,24 @@
 package IT도서.열혈자바프로그래밍.챕27_람다표현식.src.TwoParamAndReturn;
 
-interface Calculate{
-    int calculate(int a, int b);  // 값을 반환하는 추상 메소드
+@FunctionalInterface
+interface Calculable {
+    int calculate(int a, int b);
 }
 
 public class TwoParamAndReturn {
     public static void main(String[] args) {
-        Calculate calculator;
+        Calculable calculator;
 
-        calculator = (a, b) -> { return a+b ;};
-        System.out.println( calculator.calculate(3,4));
+        calculator = (a, b) -> a+b;
+        System.out.println(calculator.calculate(10, 5));
+
+        calculator = (a, b) -> a-b;
+        System.out.println(calculator.calculate(10, 5));
 
         calculator = (a, b) -> a*b;
-        System.out.println(calculator.calculate(2,5));
+        System.out.println(calculator.calculate(10, 5));
+
+        calculator = (a,b ) -> a/b;
+        System.out.println(calculator.calculate(10, 5));
     }
 }
