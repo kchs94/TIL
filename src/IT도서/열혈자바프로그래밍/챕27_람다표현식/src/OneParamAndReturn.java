dbfbe0403300fd5a401ff2e514bbb247dbde4035
김충섭 <kchs94@gmail.com>
2021-10-10 16:08:47 +0900

    Update OneParamAndReturn.java

--- src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/OneParamAndReturn.java
+++ src/IT도서/열혈자바프로그래밍/챕27_람다표현식/src/OneParamAndReturn.java
@@ -1,12 +1,12 @@
 package IT도서.열혈자바프로그래밍.챕27_람다표현식.src;
 
-interface HowLong {
-    int len(String s);  // 값을 반환하는 메소드
+interface HowLong{
+    int getLengths(String s);
 }
 
 public class OneParamAndReturn {
     public static void main(String[] args) {
-        HowLong h1 = s -> s.length();
-        System.out.println(h1.len("나는 아주 행복해"));
+        HowLong howLong = s -> s.length();
+        System.out.println(howLong.getLengths("Hello world!"));
     }
 }
